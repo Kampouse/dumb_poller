3dbb18491ea961211cf75353ca5c405a31590a47
Jean-philippe Martel <41765025+Kampouse@users.noreply.github.com>
2024-02-22 21:08:10 -0500

    ty

--- DumbedDown/utils.cpp
+++ DumbedDown/utils.cpp
@@ -5,7 +5,8 @@ StringVector split(std::string line, std::string delimiter)
 {
     StringVector result;  // Create an empty vector to store the split strings
 
-    if (line.empty())  // If the input string is empty, return an empty vector
+    // If the input string is empty, return an empty vector
+    if (line.empty())
         return result;
 
     size_t i = 0, j = 0;  // Initialize two index variables
@@ -13,9 +14,12 @@ StringVector split(std::string line, std::string delimiter)
     // Loop through the input string
     while (i < line.length() && j < line.length())
     {
-        // Find the index of the first occurrence of the delimiter starting from index j
+        // Find the index of the first occurrence of the delimiter
+        // starting from index j
         i = line.find_first_of(delimiter, j);
-        if (i == std::string::npos)  // If the delimiter is not found, set i to the end of the string
+
+        // If the delimiter is not found, set i to the end of the string
+        if (i == std::string::npos)
             i = line.length();
 
         // Extract the substring from index j to i and add it to the result vector
@@ -23,9 +27,12 @@ StringVector split(std::string line, std::string delimiter)
         if (!str.empty())
             result.push_back(str);
 
-        // Find the index of the first non-delimiter character starting from index i
+        // Find the index of the first non-delimiter character
+        // starting from index i
         j = line.find_first_not_of(delimiter, i);
     }
 
-    return result;  // Return the resulting vector of strings
+    // Return the resulting vector of strings
+    return result;
 }
+
