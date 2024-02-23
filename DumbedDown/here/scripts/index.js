35eb1ff24464acad4fcdc91888728ae907903527
Jean-philippe Martel <41765025+Kampouse@users.noreply.github.com>
2024-02-22 21:08:08 -0500

    ty

--- DumbedDown/here/scripts/index.js
+++ DumbedDown/here/scripts/index.js
@@ -1,6 +1,11 @@
 // This is a function named "sayHello"
 function sayHello() {
-  // When this function is called, it will print the string 'Hello' to the console
+  // This line uses the built-in 'console' object's 'log' method
+  // to print the string 'Hello' to the console
   console.log('Hello');
+  // A function in JavaScript is a block of code that can be executed
+  // when it is called. In this case, the function 'sayHello' can be
+  // called by using its name followed by parentheses 'sayHello()'
+  // When the function is called, it will print the string 'Hello' to the console
 }
 
