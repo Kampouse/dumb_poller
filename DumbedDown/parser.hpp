e21eb1d26ab5a6b926eed1d553df9cc10bd89dd1
Jean-philippe Martel <41765025+Kampouse@users.noreply.github.com>
2024-02-22 21:08:07 -0500

    ty

--- DumbedDown/parser.hpp
+++ DumbedDown/parser.hpp
@@ -1,38 +1,8 @@
+// parser.hpp
+
+// Include necessary header files
 #pragma once
-
-# include <fstream> 
-# include <iostream>
-# include <map>
-# include <set>
-# include <vector>
-# include <string>
-# include <stdlib.h>
-
-#include "config_structs.hpp"
-#include "Exceptions.hpp"
-#include "utils.hpp"
-
-class Exceptions;
-
-class parser
-{
-	private:
-		std::ifstream config_file_fd;
-		std::vector<server_info> servers;
-		std::vector<std::string> ext_file;
-
-		void parsefile(std::string path);
-		void extractfile(void);
-		void check_errors(void);
-		void get_server_fields(void);
-		void manage_locations(std::vector<std::string>::iterator it);
-
-	public:
-		std::vector<server_info>&get_servers(void);
-		parser();
-		parser(std::string _path);
-		~parser();
-		std::vector<server_info>&getServers();
-
-		void printfile(void);
-};
+#include <fstream>  // for file input/output
+#include <iostream> // for standard input/output operations
+#include <map>      // for storing configuration parameters
+#include <set>     
