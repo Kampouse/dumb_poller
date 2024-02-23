562544af530d50e44c9bd5a46b0f93aff9fd03dd
Jean-philippe Martel <41765025+Kampouse@users.noreply.github.com>
2024-02-22 21:08:00 -0500

    explain  what it do

--- DumbedDown/utils.hpp
+++ DumbedDown/utils.hpp
@@ -1,9 +1,38 @@
-#pragma once
+#pragma once  <-- This directive prevents the file from being included multiple times in the same compilation unit
 
-#include <vector>
-#include <cstring>
-#include <iostream>
+#include <vector>  <-- Include the vector library to create a vector of strings
+#include <cstring> <-- Include the string library for string functions
+#include <iostream> <-- Include the input/output library for console output
 
-typedef std::vector<std::string> StringVector;
+typedef std::vector<std::string> StringVector;  <-- Create a type alias for a vector of strings
 
-StringVector split(std::string line, std::string delimiter);
+// Function to split a string into a vector of strings based on a given delimiter
+StringVector split(std::string line, std::string delimiter)
+{
+    StringVector strings;  <-- Create an empty vector of strings
+
+    // Get the position of the first occurrence of the delimiter in the line
+    size_t pos = line.find(delimiter);
+
+    // While the delimiter is found in the line
+    while (pos != std::string::npos)
+    {
+        // Extract the substring up to the delimiter
+        std::string substring = line.substr(0, pos);
+
+        // Add the substring to the vector of strings
+        strings.push_back(substring);
+
+        // Remove the substring from the line
+        line.erase(0, pos + delimiter.length());
+
+        // Get the position of the next occurrence of the delimiter in the line
+        pos = line.find(delimiter);
+    }
+
+    // Add the remaining substring to the vector of strings
+    strings.push_back(line);
+
+    // Return the vector of strings
+    return strings;
+}
